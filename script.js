0a3af7fe9c4f4fed916c3bdfdbb4cc9a742afb91
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2025-12-28 03:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+	let firstName = document.querySelector(`input[name='fname']`).value;
+	let lastName = document.querySelector(`input[name='lname']`).value;
+	
+	return alert(firstName + " " + lastName);
 }
+
+	
